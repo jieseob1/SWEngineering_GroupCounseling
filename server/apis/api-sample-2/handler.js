e888c73cc8d0ebc956e05f860ec10b5b9efaf027
유주현 <gus6359@gmail.com>
2021-11-04 05:05:18 +0900

    make DB connect sample

--- server/apis/api-sample-2/handler.js
+++ server/apis/api-sample-2/handler.js
@@ -1,15 +1,33 @@
-'use strict';
+const config = {
+  host: 'groupcounseling-db.cun0dhxklfbi.us-east-1.rds.amazonaws.com',
+  user: 'gc_manager',
+  password: 'groupcounseling2021!',
+  database: 'postgres'
+}
+
+const { PostgreSQL } = require('fxsql');
+const { CONNECT } = PostgreSQL;
+
+const POOL = CONNECT(config);
+
+const { QUERY } = POOL;
 
 module.exports.hello_2 = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'sample 2 excute success!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  let error = 0;
+  try {
+    const list = await QUERY`SELECT * FROM sample`;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: list,
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    error = 1;
+  }
 };
