193255ffc211b2b2977e7999f73bfaccb89be09a
js <jieseob1@gmail.com>
2021-11-09 18:02:48 +0900

    에브리타임 클라이언트 부분 넣음

---
+++ client/src/components/Board/BoardView.js
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
+import axios from "axios";
+import styled from "styled-components";
+import StyledBox from "../Style/styledBox";
+import AddBoard from "./Section/AddBoard";
+import BoardInput from "./Section/BoardInput";
+import CheckNickname from "./Section/CheckNickname";
+import BoardTextarea from "./Section/BoardTextarea";
+import UserProfile from "./Section/UserProfile";
+import LogoutButton from "../Common/LogoutButton";
+import Header from "../Common/Header";
+import Footer from "../Common/Footer";
+import Pagination from "@material-ui/lab/Pagination";
+
+const Profilebox = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-bottom: 8px;
+`;
+const Profilebtn = styled.div`
+  display: inline-block;
+  width: 64px;
+  height: 28px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin: 12px 4px;
+  font-size: 13px;
+  line-height: 28px;
+  color: #505050;
+  pointer: cursor;
+`;
+const BoardForm = styled.form`
+  position: relative;
+  height: 165px;
+  border: 1px solid #ddd;
+  margin: 0px -1px;
+  box-sizing: border-box;
+`;
+
+const PaginationBox = styled.div`
+  text-align: center;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  display: flex;
+  justify-content: center;
+`;
+
+function BoardView({ history, match }) {
+  // 파라미터:history,match
+  const userFrom = localStorage.getItem("userId");
+  const writerFrom = localStorage.getItem("userNickname"); // writerFrom은 userNickname 관련
+  const [totalPage, settotalPage] = useState(0);//전체 페이지 설정 
+  const [currentPage, setcurrentPage] = useState(1); //현재 페이지 설정
+  const [WriterIcon, setWriterIcon] = useState(true);
+  const [BoardWriter, setBoardWriter] = useState("익명"); //게시판 적는 사람 이름
+  const [Content, setContent] = useState([]);//컨텐츠
+  const [inputs, setInput] = useState({
+    boardTitle: "",
+    boardContent: "",
+  });
+  const { boardTitle, boardContent } = inputs;
+
+  useEffect(() => {
+    FetchBoard();
+    console.log('fetch')
+  }, [currentPage]);
+
+  const FetchBoard = () => {
+    axios
+      .post("/board/getBoard", { page: currentPage }) //현재 페이지에 관련된 게시판들을 가져 온다
+      .then((response) => {
+        if (response.data.success) {
+          setContent(response.data.boards); // 성공한경우 서버에서 준 데이터 안에 있는 게시판을 가지고 와서 세팅해줌
+          settotalPage(Math.ceil(response.data.count / 5)); //소수점 이하를 반올림 한다 즉, 한페이지에 5개씩만 보여줄 예정인듯
+        } else {
+          alert("게시글을 보여줄 수 없습니다.");
+        }
+      });
+  };
+
+  const onRemove = (id) => { //
+    setContent(Content.filter((Content) => Content._id !== id)); // 컨텐츠를 filter 함수를 통해 다시 재구성한다
+    FetchBoard(); //게시판을 가지고 온다 
+  };
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInput({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onIconClick = () => { // 닉네임을 보여줄시, 익명으로 처리할지 보여주는 부분
+    if (WriterIcon) { // writerIcon이 true이게 되면
+      setWriterIcon(false); //writericon을 false로 설정하고
+      setBoardWriter(writerFrom);//닉네임을 설정하게 된다
+    } else {
+      setWriterIcon(true); //writericon이 true가 되면 글쓴이의 아이콘이 보이지 않는다.
+      setBoardWriter("익명");
+    }
+  };
+
+  const onSubmit = (e) => { //제출하는 부분
+    e.preventDefault();
+    if (!boardTitle) {
+      alert(`제목을 작성해주세요`);
+      return;
+    } else if (!boardContent) {
+      alert(`내용을 작성해주세요`);
+      return;
+    } else if (boardContent.length > 300) {
+      alert(`내용을 300자 이내로 작성해주세요`);
+      return;
+    }
+    // 유효성 검증
+
+    let variables = {
+      userFrom: userFrom,
+      boardTitle: boardTitle,
+      boardContent: boardContent,
+      boardWriter: BoardWriter,
+    }; // variable에 필요한 변수들 넣고 post로 서버에 넘겨준다
+    axios.post("/board/upload", variables).then((response) => {
+      if (response.status === 200) {
+        setInput({
+          boardTitle: "",
+          boardContent: "",
+        });
+        FetchBoard();//게시글 보여주기 
+      } else {
+        alert("게시글 업로드에 실패하였습니다.");
+      }
+    });
+  };
+
+  const handlePageChange = (e) => { //페이지 바꾸면 벌어지는 이벤트
+    const currentPage = parseInt(e.target.textContent);
+    setcurrentPage(currentPage); // ????
+  };
+  return (
+    <>
+      <Header title="자유게시판" link="/board" />
+      <StyledBox backColor="#fafafa" padding="10px 0px" lineHeight="auto">
+        <Profilebox>
+          <UserProfile boardPage={true} />
+          {/* userprofile 부분에 프로필과,아이디,학교등이 들어가게 된다. */}
+          <Link to="/mypage">
+            <Profilebtn>내정보</Profilebtn>
+          </Link>
+          <Profilebtn>
+            <LogoutButton />
+          </Profilebtn>
+        </Profilebox>
+        <BoardForm onSubmit={onSubmit}>
+          <BoardInput
+            name="boardTitle"
+            placeholder="제목을 작성해주세요."
+            value={boardTitle}
+            onChange={onChange}
+          />
+          <BoardTextarea
+            name="boardContent"
+            placeholder="여기를 눌러서 글을 작성할 수 있습니다."
+            value={boardContent}
+            onChange={onChange}
+          />
+          <CheckNickname
+            icon={WriterIcon}
+            click={onIconClick}
+            submit={onSubmit}
+          />
+        </BoardForm>
+
+
+        {Content && //아까 서버로 보낸 컨텐츠를 Content안에에다가 넣어주게 된다
+          Content.map((board, index) => {
+            return (
+              <React.Fragment key={index}>
+                <AddBoard // content 배열안에 id,user,time등 여러가지가 존재하고 그 관련된 부분들을 Addboard 컴포넌트안에 넣어준다
+                  id={board._id}
+                  user={board.userFrom._id}
+                  time={board.createdAt}
+                  writer={board.boardWriter}
+                  title={board.boardTitle}
+                  content={board.boardContent}
+                  history={`${history}`}
+                  onRemove={onRemove}
+                />
+              </React.Fragment>
+            );
+          })}
+
+
+
+        <PaginationBox>
+          <Pagination
+            count={totalPage}
+            page={currentPage}
+            onChange={handlePageChange}
+            shape="rounded"
+            size="small"
+            hidePrevButton
+            hideNextButton
+          />
+          {/* 페이지네이션 하는 부분 */}
+        </PaginationBox>
+        <Footer />
+      </StyledBox>
+    </>
+  );
+}
+
+export default withRouter(BoardView);
---
+++ client/src/pages/Board.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, withRouter } from "react-router-dom";
+import BoardDetail from "../components/Board/BoardDetail";
+// 게시판 디테일
+import BoardView from "../components/Board/BoardView";
+
+function Board({ match }) {
+  return (
+    <>
+      <Route exact path={match.path} component={BoardView} />
+      {/* 제일 처음 보이는 부분은 boardView가 된다 */}
+      <Route path={`${match.path}/:id`} component={BoardDetail} />
+      {/* baard/:id가 되며 boarddetail 부분으로 빠지게 된다 
+      :id 부분이 params가 된다*/}
+    </>
+  );
+}
+
+export default withRouter(Board);
