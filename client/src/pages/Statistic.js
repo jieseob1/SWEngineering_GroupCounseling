19055ac3db36e4755f5deb43e7c563166bd5dc59
js <jieseob1@gmail.com>
2021-12-07 00:53:31 +0900

    statistic finish

--- client/src/pages/Statistic.js
+++ client/src/pages/Statistic.js
@@ -1,10 +1,72 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import Chart from "chart.js/auto";
-import { Line } from "react-chartjs-2";
 import BarChart from "../components/Chat/Section/BarChart";
 const Statistic = () => {
-  return <BarChart />;
+  const data = [
+    {
+      date: "9/11",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/12",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/13",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/14",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/15",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/16",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/17",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/18",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/19",
+      positive: 10,
+      negative: 10,
+    },
+    {
+      date: "9/20",
+      positive: 10,
+      negative: 10,
+    },
+  ];
+
+  let startData = data.slice(0, 2);
+  let endData = data.slice(data.length - 3, data.length);
+  let new_data = [];
+
+  Object.values(startData).forEach((item) => {
+    new_data.push(item);
+  });
+  Object.values(endData).forEach((item) => {
+    new_data.push(item);
+  });
+
+  return <BarChart data={new_data} />;
 };
 
 export default withRouter(Statistic);
